72df8ef119452af3d805f3a65c225a9def2236a0
Hasan-front <84732624+Hasan-front@users.noreply.github.com>
2022-02-19 00:09:52 +0530

    College Minor

---
+++ index2.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <link rel="stylesheet" href="d2.css?v=<?php echo time(); ?>">
+
+    <title>Leos Lets educate ourselves!</title>
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="_logo1.png" height="90" width="190">
+     </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="http://www.sirifort.com/PhotoGallery.aspx">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="toursdetails.html">Tours</a>
+        </li>
+        
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Courses
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="http://www.sirifort.com/BCA.aspx">B.C.A</a></li>
+    <li><a class="dropdown-item" href="http://www.sirifort.com/BCOM.aspx">B.com</a></li>
+    <li><a class="dropdown-item" href="http://www.sirifort.com/BBA.aspx">B.B.A</a></li>
+  </ul>
+</div>
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Personal Space
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="notes.php">Notes</a></li>
+    <li><a class="dropdown-item" href="welcome3.php">Result</a></li>
+    
+  </ul>
+</div>
+         
+        
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+   <div class="main">
+            <span><h3 class="neon">About LEOS</h3></span></div>
+<p>LEOS is a web application designed to override the problems faced by students by keeping
+them updated about their college curriculum and by providing them the required study
+material with minimum system requirements.<br>
+
+Basically, helping students to keep a track on their attendance, result and to interactively
+learn, allowing students to upload their problems, notes and solutions as well.
+This web application is supported to eliminate the issues faced by college students by
+providing them a simple login to get their marksheets, results, career counselling and many
+more, also in some cases to reduce the cost of education by providing them free study
+material.
+
+In addition, having a visitor view to peak on the website without logging in by only providing
+the necessary details about the affiliated college such as college basic information, the
+courses provided by the college, the gallery view and the tours as well.
+Keeping privacy in mind Leos doesn’t allows a visitor to check in the study material provided
+and to use the personal space specially.</p>
+
+<div class="info">
+                <div>
+                    <span><h3>Courses</h3></span> <hr>
+                    <p>Still deciding what you want to study at LEOS? Browse the full range of options with our course guides, a detailed information about types of program, specializations and career prospects.</p>
+                </div>
+                <div>
+                    <span><h3>DOWNLOAD</h3></span> <hr>
+                    <p>Leos is an open sourece portal where user can dowmnload books notes and also create notes which then other students can access</p>
+                </div>
+              
+                <div>
+                    <span><h3>NOTES</h3></span> <hr>
+                    <p>The Main objective of the project is to provide study material free of cost or with minimal cost.
+•Website will manage the details of the students 
+• This website is totally built at Administrative end and thus only administrators are guaranteed  to access the data .
+• To remove the paperwork involved in learning 
+• To provide the opportunity to economically less sound students to enhance their knowledge.
+ 
+.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="footer--contact">
+                <p>Contact Us</p>
+                <p>hfiroz48@gmail.com</p>
+                <p>garvitasapra99@gmail.com</p>
+            </div>
+            <div class="footer--info">
+                <span>Important Links</span>
+                <a href="index2.php">Home</a>
+                <a href="login.php">Admin Login</a>
+                <a href="login.php">Results</a>
+            </div>
+        </div>
+
+    </div>
+  </body>
+</html>
